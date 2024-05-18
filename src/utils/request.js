19eb763023840448ef9650714ae2ba2fcffc867e
baijing0328 <96113803+baijing0328@users.noreply.github.com>
2024-05-18 14:44:40 +0800

    feat: save

---
+++ src/utils/request.js
@@ -0,0 +1,32 @@
+import axios from 'axios'
+import { ElMessage } from 'element-plus'
+
+const baseURL = 'http://localhost:3000'
+
+const instance = axios.create({
+    baseURL,
+    timeout: 10000
+})
+
+
+// 响应拦截器
+instance.interceptors.response.use(
+    (res) => {
+        if (res.data.code === 0) {
+            return res
+        }
+        ElMessage.error(res.data.message || '服务异常')
+        return Promise.reject(res.data)
+    },
+    (err) => {
+        //不知道有没有登陆的逻辑
+        //     if (err.response?.status === 401) {
+        //     router.push('/login')
+        //   }
+        ElMessage.error(err.response.data.message || '服务异常')
+        return Promise.reject(err)
+    }
+)
+
+export default instance
+export { baseURL }
