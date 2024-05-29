b32c0610138b3d2b21aa764292ca19f951cd28c9
baijing0328 <96113803+baijing0328@users.noreply.github.com>
2024-05-29 19:33:12 +0800

    change config

--- src/main.js
+++ src/main.js
@@ -2,12 +2,21 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import 'element-plus/theme-chalk/src/message.scss'
+import { initAMapApiLoader } from '@vuemap/vue-amap'
+import '@vuemap/vue-amap/dist/style.css'
 
 import App from './App.vue'
 import router from './router'
 //注释掉下面这行
 import '@/mock/index'
 import '@/assets/main.scss'
+
+initAMapApiLoader({
+  key: 'YOUR_KEY'
+  //Loca:{
+  //  version: '2.0.0'
+  //} // 如果需要使用loca组件库，需要加载Loca
+})
 
 const app = createApp(App)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
